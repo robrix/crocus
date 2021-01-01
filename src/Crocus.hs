d8233bda23aabf872017428ad8f39c058f6110f5
Rob Rix <rob.rix@me.com>
2021-01-01 15:56:35 -0500

    Stub in a simple API.

--- src/Crocus.hs
+++ src/Crocus.hs
@@ -1,2 +1,33 @@
 module Crocus
 () where
+
+import Control.Carrier.NonDet.Church
+import Data.Functor.Identity
+
+type Relation = String
+
+type Constant = String
+
+-- type Variable = String
+
+fact :: Relation -> [Constant] -> Program ()
+fact _ _ = Program (pure ())
+
+-- rule :: Relation -> [Either Variable Constant] -> Program ()
+
+-- query ::
+
+
+newtype Program a = Program (NonDetC Identity a)
+  deriving (Applicative, Functor, Monad)
+
+x = do
+  fact "report" ["doug", "ayman"]
+  fact "report" ["doug", "beka"]
+  fact "report" ["doug", "max"]
+  fact "report" ["doug", "patrick"]
+  fact "report" ["doug", "rob"]
+  fact "report" ["doug", "rick"]
+  fact "report" ["doug", "tim"]
+
+-- -- rule "coworker" $ \ _X ->
