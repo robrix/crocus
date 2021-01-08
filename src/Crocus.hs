bac2af02b2c0c893698013f7ed668836394dee64
Rob Rix <rob.rix@me.com>
2021-01-08 12:23:09 -0500

    Define a generic handler.

--- src/Crocus.hs
+++ src/Crocus.hs
@@ -218,6 +218,13 @@ instance MSplit [] where
     []  -> [Nothing]
     h:t -> [Just (h, t)]
 
+runL :: MSplit m => m a -> m [a]
+runL m = do
+  r <- msplit m
+  case r of
+    Nothing      -> pure []
+    Just (a, m') -> (a:) <$> runL m'
+
 
 -- data Decl where
 --   Letrec :: (Expr -> Expr) -> (Expr -> Decl) -> Decl
